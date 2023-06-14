245d74154a7ea4ef91604a0ea6ebbbae2b2f561d
Felipecdc <124524102+Felipecdc@users.noreply.github.com>
2023-06-14 20:58:23 -0300

    Add files via upload

---
+++ Nota_Supermercado/supermercado.js
@@ -0,0 +1,48 @@
+const Items = [];
+
+document.querySelector('input[type=submit]')
+.addEventListener('click',()=>{
+    const nomeProduto = document.querySelector('input[name=nome_produto]');
+    const precoProduto = document.querySelector('input[name=price]');
+
+    Items.push({
+        nome: nomeProduto.value,
+        valor: precoProduto.value,
+    });
+
+    /*
+    <div class="lista-produtos-single">
+        <h3>RedBull</h3>
+        <h3 class="price-produto"><span>R$20,00</span></h3>
+    </div>
+    */
+
+    let listaProdutos = document.querySelector('.lista-produtos')
+    let soma = 0;
+    
+    listaProdutos.innerHTML = "";
+    Items.map(function(val){
+        soma+=parseFloat(val.valor);
+        listaProdutos.innerHTML+=`
+        <div class="lista-produtos-single">
+            <h3>`+val.nome+`</h3>
+            <h3 class="price-produto"><span>`+val.valor+`</span></h3>
+        </div>
+        `;
+    })
+
+    soma = soma.toFixed(2);
+    nomeProduto.value = "";
+    precoProduto.value = "";
+
+    let elementoSoma = document.querySelector('.soma-produto h1');
+    elementoSoma.innerHTML = "R$"+soma;
+});
+
+document.querySelector('button[name=limpar]')
+.addEventListener('click', ()=>{
+    Item = [];
+    document.querySelector('.lista-produtos').innerHTML = "";
+    document.querySelector('.soma-produto h1').innerHTML = "R$0,00";
+
+})
