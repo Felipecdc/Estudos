8f29f79b4d16ebbbb8c458765bf840efaa694e79
Felipecdc <124524102+Felipecdc@users.noreply.github.com>
2023-06-19 19:01:17 -0300

    Update script.js

--- Estudos.JS/index.2/script.js
+++ Estudos.JS/index.2/script.js
@@ -1,18 +1,24 @@
-// setInterval = Execuções definidas sequenciais de tempo em tempo determinado
 
 /*
 function acao(){
     document.write('Executando... <br>')
 }
+*/
+
+
+/*
+// setInterval = Execuções definidas sequenciais de tempo em tempo determinado
 
 setInterval(() => {
     document.write('Executando... !!!!! <br>')
 },2000)
 */
 
-
+/*
 //setTimeout = Executas apenas uma vez o que for passado
 
 setTimeout(() => {
     document.write('Executando... <br>')
 }, 3000)
+
+*/
