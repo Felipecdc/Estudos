4fac10eac599a7a23e61c35329f1ff681cf5d692
Felipecdc <124524102+Felipecdc@users.noreply.github.com>
2023-06-19 19:00:10 -0300

    Add files via upload

---
+++ Estudos.JS/index.2/script.js
@@ -0,0 +1,18 @@
+// setInterval = Execuções definidas sequenciais de tempo em tempo determinado
+
+/*
+function acao(){
+    document.write('Executando... <br>')
+}
+
+setInterval(() => {
+    document.write('Executando... !!!!! <br>')
+},2000)
+*/
+
+
+//setTimeout = Executas apenas uma vez o que for passado
+
+setTimeout(() => {
+    document.write('Executando... <br>')
+}, 3000)
